a44ce8aecdf55ba650fbd594c4accc01e44e87c8
David Reyna <David Reyna@DESKTOP-53NO1KM>
2019-03-07 12:53:12 -0500

    Mi primer clonacion

---
+++ src/ejemplo/pe/upeu/edu/pe/Prueba.java
@@ -0,0 +1,10 @@
+package ejemplo.pe.upeu.edu.pe;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Este es una Prueba de GIT-HUB");
+	}
+
+}
